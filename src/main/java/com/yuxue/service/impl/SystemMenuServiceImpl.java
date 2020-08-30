d793d49f77f7408ff60f0469b8406b70242b8d4e
yuxue <yuxue1687@qq.com>
2020-08-30 11:35:15 +0800

    no commit message

--- src/main/java/com/yuxue/service/impl/SystemMenuServiceImpl.java
+++ src/main/java/com/yuxue/service/impl/SystemMenuServiceImpl.java
@@ -76,44 +76,9 @@ public class SystemMenuServiceImpl  implements SystemMenuService {
 	
 	@Override
     public Object getUserMenu() {
-        SystemMenuEntity m1 = new SystemMenuEntity();
-        m1.setId(1);
-        m1.setMenuName("车牌识别");
-        m1.setMenuUrl("../../templates/plate/content.html");
-        m1.setParentId(0);
-        m1.setSort(1);
-        m1.setMenuLevel(1);
-        m1.setMenuIcon("ion-flask");
-        m1.setShowFlag(1);
-        
-        SystemMenuEntity m2 = new SystemMenuEntity();
-        m2.setId(1);
-        m2.setMenuName("人脸识别");
-        m2.setMenuUrl("../../templates/face/content.html");
-        m2.setParentId(0);
-        m2.setSort(2);
-        m2.setMenuLevel(1);
-        m2.setMenuIcon("ion-flask");
-        m2.setShowFlag(1);
-        
-        SystemMenuEntity m3 = new SystemMenuEntity();
-        m3.setId(1);
-        m3.setMenuName("证件识别");
-        m3.setMenuUrl("../../templates/card/content.html");
-        m3.setParentId(0);
-        m3.setSort(3);
-        m3.setMenuLevel(1);
-        m3.setMenuIcon("ion-flask");
-        m3.setShowFlag(1);
-        
-        List<SystemMenuEntity> menus = Lists.newArrayList();
-        menus.add(m1);
-        menus.add(m2);
-        menus.add(m3);
-        
-        /*Map<String, Object> map = Maps.newHashMap();
+        Map<String, Object> map = Maps.newHashMap();
         map.put("showFlag", 1);
-        List<SystemMenuEntity> menus = systemMenuMapper.selectByCondition(map);*/
+        List<SystemMenuEntity> menus = systemMenuMapper.selectByCondition(map);
         
         //按层级封装，最多三级
         Map<String, Object> result = Maps.newHashMap();
