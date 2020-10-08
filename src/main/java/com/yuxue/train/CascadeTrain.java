4f1c678d2b2d5b4226a75db11c0277b846274d5c
yuxue <yuxue1687@qq.com>
2020-10-08 18:19:01 +0800

    级联分类器模型训练

--- src/main/java/com/yuxue/train/CascadeTrain.java
+++ src/main/java/com/yuxue/train/CascadeTrain.java
@@ -1,5 +1,12 @@
 package com.yuxue.train;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
 import java.util.Vector;
 
 import org.opencv.core.Core;
@@ -8,25 +15,27 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
+import com.google.common.collect.Maps;
 import com.yuxue.constant.Constant;
 import com.yuxue.util.FileUtil;
 import com.yuxue.util.ImageUtil;
 
 /**
- * 人脸识别训练、检测 (级联分类器)  未完成
+ * 人脸识别训练、检测 (级联分类器)
  * 
  * 训练自己的级联分类器 -- 调用opencv的exe应用程序
  * https://blog.csdn.net/dbzzcz/article/details/105517946
- * 获取opencv_traincascade.exe应用程序，官方下载3.4.1版本的安装包，解压出来即可找到
- * 或者加入 1054836232 Q群,在群共享文件中获取应用程序安装包
- * 
  *  官方教程地址：
  *  https://docs.opencv.org/master/dc/d88/tutorial_traincascade.html
+ * 
+ * 获取opencv_traincascade.exe应用程序，官方下载3.4.1版本的安装包，解压出来即可找到
+ * 或者加入 1054836232 Q群, 在群共享文件中获取应用程序安装包
  * @author yuxue
  * @date 2020-09-15 12:32
  */
@@ -37,8 +46,22 @@ public class CascadeTrain {
     }
 
     // 默认的训练操作的根目录
-    private static final String DEFAULT_PATH = "D:/FaceDetect/train/";
-    private static final String MODEL_PATH = DEFAULT_PATH + "haarcascade_frontalface_my.xml";
+    private static final String DEFAULT_PATH = "D:/FaceDetect/";
+
+    // 应用程序所在目录
+    private static final String EXE_BASE_PATH = "D:/OpenCV/3.4.1/build/x64/vc14/bin/";
+    private static final String EXE_TRAINCASCADE= EXE_BASE_PATH  + "opencv_traincascade.exe";
+    private static final String EXE_CREATESAMPLES= EXE_BASE_PATH  + "opencv_createsamples.exe";
+
+    // 
+    private static Integer width = 24;
+    private static Integer height = 24;
+
+    private static final String posInfo = "pos.info"; // 正样本文件路径数据，一行代表一个文件
+    private static final String negInfo = "neg.info"; // 负样本文件路径数据，一行代表一个文件
+    
+    private static final String posVec = "pos.vec"; // 包含正样本的vec文件；  cmd_createsample 生成
+
     
     /**
      * 生成文件名称
@@ -54,44 +77,42 @@ public class CascadeTrain {
      * 训练出来的模型文件，用于识别图片中是否包含人脸
      */
     public static void train() {
-        // 正负样本文件路径
-        String negative = "D:\\FaceDetect\\samples\\negative\\";
-        String positive = "D:\\FaceDetect\\samples\\positive\\";
-
-        Mat samples = new Mat(); // 使用push_back，行数列数不能赋初始值
-        Vector<Integer> labels = new Vector<Integer>();
-
-        // 加载正负样本文件
-        loadSamples(samples, labels, negative, positive);
-        
-        // 开始训练 // 使用opencv提供的训练器，命令方式启动
-        String _cascadeDirName = "";
-        String _posFilename = ""; 
-        String _negFilename = "";
-        int _numPos = 2000;
-        int _numNeg = 1000;
-        int _precalcValBufSize = 1024; 
-        int _precalcIdxBufSize = 1024;
-        int _numStages = 20;
-
-        boolean baseFormatSave = false;
-        double acceptanceRatioBreakValue = -1.0;
-        
-        /*CvCascadeParams _cascadeParams = new CvCascadeParams();
-        CvFeatureParams _featureParams = new CvFeatureParams();
-        CvCascadeBoostParams _stageParams = new CvCascadeBoostParams();
-
-        CascadeClassifier faceDetector = new CascadeClassifier(DEFAULT_PATH + "haarcascade_frontalface_my.xml");
-        faceDetector.train(_cascadeDirName, _posFilename,
-                _negFilename, _numPos, _numNeg, 
-                _precalcValBufSize, _precalcIdxBufSize, 
-                _numStages, _cascadeParams, _featureParams, _stageParams, baseFormatSave, acceptanceRatioBreakValue);*/
-        
-        return ;
+        Map<String, Object> param = Maps.newLinkedHashMap();
+        param.put("data", DEFAULT_PATH + "samples/data/");
+        param.put("vec", DEFAULT_PATH + "samples/" + posVec);
+        param.put("bg", DEFAULT_PATH + "samples/" + negInfo);
+        param.put("numPos", 16);
+        param.put("numNeg", 16);
+        param.put("numStages", 16);
+        param.put("minhitrate", 0.99);
+        param.put("maxfalsealarm", 0.45);
+        param.put("featureType", "LBP"); // LBP
+        param.put("mode", "ALL");
+        param.put("w", width);
+        param.put("h", height);
+        param.put("precalcValBufSize", 2048);
+        param.put("precalcIdxBufSize", 2048);
+        execCmd(EXE_TRAINCASCADE, param);   // 这步操作需要复制控制台输出的命令到cmd窗口下执行，具体原因暂未定位
+        // traincascade's error (Required leaf false alarm rate achieved. Branch training terminated.)
+        // 先测试一下生成的cascade.xml，如果效果没有达到你的预期，
+        // 解决方案： 1：maxfalsealarm值应该设定到0.4 - 0.5之间  2：正负样本数太少，增大样本数
     }
     
-    
-    
+    public static void execCmd(String cmd, Map<String, Object> param) {
+        try {
+            // 遍历参数
+            for (Entry<String, Object> entry : param.entrySet()) {
+                cmd = cmd + " -" + entry.getKey() + " " + entry.getValue();
+            }
+            System.out.println(cmd);
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+
+
     /**
      * 加载样本文件
      * @param samples
@@ -99,43 +120,86 @@ public class CascadeTrain {
      * @param negative
      * @param positive
      */
-    public static void loadSamples(Mat samples, Vector<Integer> labels, String negative, String positive) {
+    public static void loadSamples(String negative, String positive) {
         // 加载负样本
-        Vector<String> files = new Vector<String>();
-        FileUtil.getFiles(negative, files);
-        for (String img : files) {
-            Mat inMat = Imgcodecs.imread(img);
-            samples.push_back(inMat);
-            labels.add(0);
+        File f = new File(negative);
+        File negInfoFile = new File( f.getParent().concat("/").concat(negInfo));
+        FileWriter fw;
+        try {
+            fw = new FileWriter(negInfoFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (File img : f.listFiles()) {
+                bw.write(img.getPath()); // 使用 绝对 路径
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
+
         // 加载正样本
-        files = new Vector<String>();
-        FileUtil.getFiles(positive, files);
-        for (String img : files) {
-            Mat inMat = Imgcodecs.imread(img);
-            samples.push_back(inMat);
-            labels.add(1);
+        f = new File(positive);
+        File posInfoFile = new File(f.getParent().concat("/").concat(posInfo));
+        try {
+            fw = new FileWriter(posInfoFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (File img : f.listFiles()) {
+                bw.write(f.getName() + "\\" +  img.getName() + " 1 0 0 " + width + " " + height); // 使用相对路径
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
+        
+        Map<String, Object> param = Maps.newLinkedHashMap();
+        param.put("info", DEFAULT_PATH + "samples/" + posInfo);
+        param.put("vec", DEFAULT_PATH + "samples/" + posVec);
+        param.put("num", f.listFiles().length);
+        param.put("w", width);
+        param.put("h", height);
+        execCmd(EXE_CREATESAMPLES, param);
     }
 
-    
+
     /**
      * 预测图片中是否包含人脸
      * 加载训练完成的模型文件，识别测试图片中是否包含人脸
      */
     public static void predict() {
+        // 训练结果文件路径
+        String trainedModel = DEFAULT_PATH + "samples/data/cascade.xml";
+        CascadeClassifier faceDetector = new CascadeClassifier(trainedModel);
+        Mat inMat = Imgcodecs.imread(DEFAULT_PATH+ "train/huge/huge.png");
+        String targetPath =  DEFAULT_PATH + "samples/result.jpg";
         
+        Boolean debug = false;
+        Mat grey = ImageUtil.gray(inMat, debug, DEFAULT_PATH);
+        Mat gsBlur = ImageUtil.gaussianBlur(grey, debug, DEFAULT_PATH);
+
+        MatOfRect faceDetections = new MatOfRect(); // 识别结果存储对象 // Rect矩形类
+        faceDetector.detectMultiScale(gsBlur, faceDetections); // 识别人脸
+        System.out.println(String.format("识别出 %s 张人脸", faceDetections.toArray().length));
+        
+        // 在识别到部位描框
+        for (Rect rect : faceDetections.toArray()) {
+            Imgproc.rectangle(inMat, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+            Imgcodecs.imwrite(targetPath, inMat);
+        }
+        return;
     }
-    
+
 
     /**
-     * 从图片中，提取人脸图块(正样本)，或者疑似人脸的非人脸图块(负样本)
-     * 将图块处理为相同大小正负样本文件，放到样本训练目录下
+     * 从图片中，提取人脸图块(正样本)
+     * 将图块处理为相同大小正样本文件，放到样本训练目录下
      * @param sourcePath 原图目录
      * @param targetPath 样本存放目录
      * @param limit 提取样本数量
      */
-    public static void prepareSamples(String sourcePath, String targetPath, Integer limit) {
+    public static void preparePosSamples(String sourcePath, String targetPath, Integer limit) {
         Vector<String> files = new Vector<String>();
         FileUtil.getFiles(sourcePath, files);
         CascadeClassifier faceDetector = new CascadeClassifier(Constant.DEFAULT_FACE_MODEL_PATH);
@@ -146,24 +210,23 @@ public class CascadeTrain {
                 continue;
             }
             Mat gray = ImageUtil.gray(inMat, false, "");
-            Mat gsMat = ImageUtil.gaussianBlur(gray, false, "");
+            // Mat gsMat = ImageUtil.gaussianBlur(gray, false, "");
             MatOfRect faceDetections = new MatOfRect();
-            faceDetector.detectMultiScale(gsMat, faceDetections);
+            faceDetector.detectMultiScale(gray, faceDetections);
             for (Rect rect : faceDetections.toArray()) {
                 // 截取人脸  灰度图
                 Mat face = new Mat();
                 Size size = new Size(rect.width, rect.height);
                 Point center = new Point(rect.x + rect.width/2, rect.y + rect.height/2);
-                Imgproc.getRectSubPix(gsMat, size, center, face);
+                Imgproc.getRectSubPix(gray, size, center, face);
 
                 // resize 24*24
-                Mat resized = new Mat(24, 24, CvType.CV_8UC3);
+                Mat resized = new Mat(width, height, CvType.CV_8UC3);
                 Imgproc.resize(face, resized, resized.size(), 0, 0, Imgproc.INTER_CUBIC);
                 // 保存文件
                 Imgcodecs.imwrite(targetPath + getId() + ".jpg", resized);
-                
                 i++;
-                if(i >= 2) {
+                if(i >= limit) {
                     return;
                 }
             }
@@ -172,32 +235,64 @@ public class CascadeTrain {
     }
 
 
+    /**
+     * 从图片中，随机提取负样本； 放到样本训练目录下
+     * @param sourcePath 原图目录
+     * @param targetPath 样本存放目录
+     * @param limit 提取样本数量
+     */
+    public static void prepareNegSamples(String sourcePath, String targetPath, Integer limit) {
+        Vector<String> files = new Vector<String>();
+        FileUtil.getFiles(sourcePath, files);
+        int i = 0;
+        Random rd = new Random();
+        for (String img : files) {
+            Mat inMat = Imgcodecs.imread(img);
+            if(inMat.empty()) {
+                continue;
+            }
+            Mat gray = ImageUtil.gray(inMat, false, "");
+            Mat gsMat = ImageUtil.gaussianBlur(gray, false, "");
+            // 随机截取指定大小的图片，用作负样本
+            Mat negMat = new Mat();
+            Size size = new Size(width, height);
+            Point center = new Point(rd.nextInt(inMat.cols()- 101) + 100, rd.nextInt(inMat.rows()- 101) + 100);
+            Imgproc.getRectSubPix(gsMat, size, center, negMat);
+            // 保存文件
+            Imgcodecs.imwrite(targetPath + getId() + ".jpg", negMat);
+            i++;
+            if(i >= limit) {
+                return;
+            }
+        }
+        return;
+    }
+
+
+
     public static void main(String[] args) {
 
-        /*String sourcePath = "D:\\FaceDetect\\samples\\asia_all\\";
-        String targetPath = "D:\\FaceDetect\\samples\\positive\\";
-        // prepareSamples(sourcePath, targetPath, 4000);
-
-        sourcePath = "D:\\FaceDetect\\samples\\europe_male\\";
-        targetPath = "D:\\FaceDetect\\samples\\positive\\";
-        // prepareSamples(sourcePath, targetPath, 2000);
-
-        sourcePath = "D:\\FaceDetect\\samples\\europe_female\\";
-        targetPath = "D:\\FaceDetect\\samples\\positive\\";
-        // prepareSamples(sourcePath, targetPath, 2000);
-
+        String sourcePath = DEFAULT_PATH + "samples\\asia_all\\";
+        String targetPath = DEFAULT_PATH + "samples\\positive\\";
+        // preparePosSamples(sourcePath, targetPath, 4000);
+        
         sourcePath = "D:\\PlateDetect\\plate\\";
         targetPath = "D:\\FaceDetect\\samples\\negative\\";
-        // prepareSamples(sourcePath, targetPath, 4000);
+        int i = 0;
+        do {
+            // prepareNegSamples(sourcePath, targetPath, 400);
+            i++;
+        } while (i<=100);
 
-        sourcePath = "D:\\PlateDetect\\plate1\\";
-        targetPath = "D:\\FaceDetect\\samples\\negative\\";
-        // prepareSamples(sourcePath, targetPath, 4000);
+
+        String negative = DEFAULT_PATH + "samples\\negative\\";
+        String positive = DEFAULT_PATH + "samples\\positive\\";
+        // loadSamples(negative, positive);
         
-        train();
+        // train();
         
-        predict();*/
-        
+        predict();
+
         return;
     }
 
